99951bf72b621d6780dcba679f77b6d5d3e141c4
Dominic Mejia <206021858+dominatormejia@users.noreply.github.com>
2025-07-04 21:55:26 -0700

    Sidebar drop down with sub-drop downs

--- src/index.js
+++ src/index.js
@@ -7,9 +7,21 @@ const button = document.querySelector("button");
 const dropDown = document.querySelector(".drop-down");
 
 button.addEventListener("click", () => {
-  if (dropDown.classList.contains("hidden")) {
-    dropDown.classList.remove("hidden");
-  } else {
-    dropDown.classList.add("hidden");
+  if (dropDown) {
+    dropDown.classList.toggle("hidden");
+  }
+});
+
+dropDown.addEventListener("click", (event) => {
+  if (event.target.classList.contains("sidebar")) {
+    const sibling = event.target.nextElementSibling;
+    const child = event.target.firstElementChild;
+    if (sibling) {
+      sibling.classList.toggle("hidden");
+    }
+    if (child) {
+      child.classList.toggle("down");
+      child.classList.toggle("up");
+    }
   }
 });
